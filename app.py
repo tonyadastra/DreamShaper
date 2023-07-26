14ddebe668d8d4622281d8af5ec275182a690143
Manoj Kumar <manoj@inferless.com>
2023-07-26 05:04:55 +0000

    Added Template

---
+++ app.py
@@ -0,0 +1,23 @@
+import torch
+from io import BytesIO
+import base64
+from diffusers import DiffusionPipeline
+
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.pipe = DiffusionPipeline.from_pretrained(
+            "Lykon/DreamShaper", 
+            torch_dtype=torch.float16
+        )
+        self.pipe = self.pipe.to("cuda:0")
+
+    def infer(self, prompt):
+        image = self.pipe(prompt).images[0]
+        buff = BytesIO()
+        image.save(buff, format="JPEG")
+        img_str = base64.b64encode(buff.getvalue())
+        return img_str
+
+    def finalize(self):
+        self.pipe = None
