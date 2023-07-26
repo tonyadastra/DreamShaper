95171d3b5a9e94291c897ae0d53244f3ba6a3c5b
Nilesh Agarwal <nick.aggarwal@gmail.com>
2023-07-26 22:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,72 @@ import torch
 from io import BytesIO
 import base64
 from diffusers import DiffusionPipeline
+import PIL.Image
+import base64
+import io
+from diffusers import ControlNetModel, StableDiffusionControlNetPipeline
 
 
 class InferlessPythonModel:
     def initialize(self):
-        self.pipe = DiffusionPipeline.from_pretrained(
-            "Lykon/DreamShaper", 
-            torch_dtype=torch.float16
+        brightness_controlnet = ControlNetModel.from_pretrained(
+            "ioclab/control_v1p_sd15_brightness", torch_dtype=torch.float16
         )
-        self.pipe = self.pipe.to("cuda:0")
+
+        tile_controlnet = ControlNetModel.from_pretrained(
+            "lllyasviel/control_v11f1e_sd15_tile",
+            torch_dtype=torch.float16,
+            use_safetensors=False,
+        )
+
+        controller = StableDiffusionControlNetPipeline.from_pretrained(
+            "Lykon/DreamShaper",
+            controlnet=[brightness_controlnet, tile_controlnet],
+            torch_dtype=torch.float16,
+            use_safetensors=False,
+            safety_checker=None,
+        ).to("cuda")
+
+        controller.enable_xformers_memory_efficient_attention()
+        self.pipe = controller
+
+    def resize_for_condition_image(input_image, resolution: int = 512):
+        import PIL.Image
+
+        input_image = input_image.convert("RGB")
+        W, H = input_image.size
+        k = float(resolution) / min(H, W)
+        H *= k
+        W *= k
+        H = int(round(H / 64.0)) * 64
+        W = int(round(W / 64.0)) * 64
+        img = input_image.resize((W, H), resample=PIL.Image.LANCZOS)
+        return img
 
     def infer(self, inputs):
+
+        input_image = inputs["input_image"]
+        print(input_image[:10])
+        print(input_image[-10:])
+        if "base64," in input_image:
+            input_image = input_image.split("base64,")[1]
+        input_image = PIL.Image.open(io.BytesIO(base64.b64decode(input_image))).convert(
+            "RGB"
+        )
+        input_image = input_image.resize((512, 512), resample=PIL.Image.LANCZOS)
+        tile_input_image = self.resize_for_condition_image(input_image)
+        
         prompt = inputs["prompt"]
-        image = self.pipe(prompt).images[0]
+        image = self.pipe(
+            text,
+            image=[input_image, tile_input_image],
+            height=512,
+            width=512,
+            num_inference_steps=100,
+            controlnet_conditioning_scale=[0.45, 0.25],
+            guidance_scale=9,
+        )["images"][0]
+        
         buff = BytesIO()
         image.save(buff, format="JPEG")
         img_str = base64.b64encode(buff.getvalue())
